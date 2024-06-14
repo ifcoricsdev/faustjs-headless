24ace21669ed4b3b0007e81e9b6819c145bcd1b1
ifcoricsdev <rico@inboundfound.com>
2024-06-14 16:05:05 +0800

    getting correct path url(s)

--- utils/static-cards/getBackgroundPath.js
+++ utils/static-cards/getBackgroundPath.js
@@ -4,6 +4,12 @@ export const getBackgroundPath = () => {
       '.if-dynamic-card.card-type-post[data-post_type="post"]'
     );
 
+    const ibfModal = document.querySelectorAll('.expertise-cards-icon > img');
+
+    const eventCardsImages = document.querySelectorAll(
+      '.events-cards-group .image img'
+    );
+
     posts.forEach((post) => {
       const backgroundImage = post.style.backgroundImage;
       const newBackgroundImage = backgroundImage.replace(
@@ -11,6 +17,24 @@ export const getBackgroundPath = () => {
         'url("./$1")'
       );
       post.style.backgroundImage = newBackgroundImage;
+    });
+
+    ibfModal.forEach((modal) => {
+      const backgroundSrc = modal.src;
+      const newBackgroundSrc = backgroundSrc.replace(
+        /\/wp-content\/plugins\//,
+        '/'
+      );
+      modal.src = newBackgroundSrc;
+    });
+
+    eventCardsImages.forEach((images) => {
+      const backgroundSrc = images.src;
+      const newBackgroundSrc = backgroundSrc.replace(
+        /\/wp-content\/plugins\/if-blocks-main\//,
+        '/'
+      );
+      images.src = newBackgroundSrc;
     });
   }
 };
